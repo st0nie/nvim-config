89bf5ccb5de09a2f3113025206ad176960af0cdf
ston <2424284164@qq.com>
2022-11-25 22:47:11 +0800

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -131,12 +131,12 @@ return require("packer").startup(function()
 		end,
 	})
 	use({ "onsails/lspkind.nvim" })
-	use {
-		'lewis6991/gitsigns.nvim',
+	use({
+		"lewis6991/gitsigns.nvim",
 		config = function()
-			require('gitsigns').setup {
+			require("gitsigns").setup({
 				current_line_blame = true,
-			}
-		end
-	}
+			})
+		end,
+	})
 end)
