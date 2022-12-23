6d3d903fcbac74b3e21f4970f9fbeee6b92ca021
ston <2424284164@qq.com>
2022-12-23 22:30:45 +0800

    add gopls

--- lua/plugconf/lsp.lua
+++ lua/plugconf/lsp.lua
@@ -1,4 +1,4 @@
-local servers = { "pyright", "sumneko_lua", "clangd", "rust_analyzer", "bashls", "marksman" }
+local servers = { "pyright", "sumneko_lua", "clangd", "rust_analyzer", "bashls", "marksman", "gopls" }
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for i = 1, #servers do
 	require("lspconfig")[servers[i]].setup({
