7582f65d176cd53bc8bd6339171330145ce3b392
ston <ston.jia@qq.com>
2023-02-27 21:11:04 +0800

    fix session

--- lua/plugconf/session.lua
+++ lua/plugconf/session.lua
@@ -6,7 +6,12 @@ require("session_manager").setup({
 	autoload_mode = require("session_manager.config").AutoloadMode.CurrentDir, -- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
 	autosave_last_session = true, -- Automatically save last session on exit and on session switch.
 	autosave_ignore_not_normal = true, -- Plugin will not save a session when no buffers are opened, or all of them aren't writable or listed.
-	autosave_ignore_dirs = { "~/", "~/Projects", "~/Downloads", "/" }, -- A list of directories where the session will not be autosaved.
+	autosave_ignore_dirs = {
+		vim.fn.expand("$HOME/Workspaces"),
+		vim.fn.expand("$HOME/Projects"),
+		vim.fn.expand("$HOME"),
+		"/",
+	}, -- A list of directories where the session will not be autosaved.
 	autosave_ignore_filetypes = { -- All buffers of these file types will be closed before the session is saved.
 		"gitcommit",
 	},
