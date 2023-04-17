97820e60e92df3866220f39c32d86e30cd073c30
ston <ston.jia@qq.com>
2023-04-17 20:24:39 +0800

    prevent lua_ls prompt
    Signed-off-by: ston <ston.jia@qq.com>

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -13,8 +13,32 @@ require("lspconfig").clangd.setup({
 local servers = { "pyright", "lua_ls", "clangd", "rust_analyzer", "bashls", "marksman", "gopls" }
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 for i = 1, #servers do
-	require("lspconfig")[servers[i]].setup({
-		capabilities = capabilities,
-		on_attach = on_attach,
-	})
+	if servers[i] == "lua_ls" then
+		require("lspconfig")[servers[i]].setup({
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+	else
+		require("lspconfig")[servers[i]].setup({
+			capabilities = capabilities,
+			on_attach = on_attach,
+		})
+	end
 end
