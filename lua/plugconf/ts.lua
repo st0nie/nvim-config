f72601bbac16ac8f3e564ad15f6ecc28c7413b10
ston <ston.jia@qq.com>
2023-04-17 19:12:15 +0800

    add ufo_conf for better fold
    Signed-off-by: ston <ston.jia@qq.com>

--- lua/plugconf/ts.lua
+++ lua/plugconf/ts.lua
@@ -29,7 +29,3 @@ require("nvim-treesitter.configs").setup({
 		enable = true,
 	},
 })
-
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
 		"folke/which-key.nvim",
 		event = "VeryLazy",
 		init = function()
+			vim.g.maplocalleader = " "
 			vim.g.mapleader = " "
 		end,
 		config = function()
@@ -318,5 +319,37 @@ require("lazy").setup({
 				end,
 			},
 		},
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					})
+				end,
+			},
+		},
+		init = function()
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+		end,
+		config = function()
+			require("plugconf.ufo_conf")
+		end,
 	},
 })
