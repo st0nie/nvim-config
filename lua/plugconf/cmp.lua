e1631039b3e08259474b5eb894ba21e7ee3c015b
ston <ston.jia@qq.com>
2023-04-23 17:04:59 +0800

    only jump in region
    Signed-off-by: ston <ston.jia@qq.com>

--- lua/plugconf/cmp.lua
+++ lua/plugconf/cmp.lua
@@ -72,7 +72,7 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.confirm()
-			elseif luasnip.expand_or_jumpable() then
+			elseif luasnip.expand_or_locally_jumpable() then
 				luasnip.expand_or_jump()
 			elseif has_words_before() then
 				cmp.complete()
