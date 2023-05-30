8d761e5060ede667a33445fc66190d95b48427ee
ston <ston.jia@qq.com>
2023-05-30 17:07:55 +0800

    add surround
    Signed-off-by: ston <ston.jia@qq.com>

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -385,4 +385,14 @@ require("lazy").setup({
 			require("satellite").setup()
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 })
