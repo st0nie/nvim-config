b89098d8809e59527d549b66dc282996bec73c75
ston <ston.jia@outlook.com>
2024-03-25 18:20:13 +0800

    update skeleton
    Signed-off-by: ston <ston.jia@outlook.com>

--- lua/skeleton.lua
+++ lua/skeleton.lua
@@ -3,17 +3,17 @@ vim.api.nvim_create_autocmd({ "BufNewFile" }, {
 	pattern = "*.ebuild",
 	callback = function()
 		vim.cmd("0r ~/Templates/skeleton.ebuild")
-		vim.cmd("0r /var/db/repos/gentoo/header.txt")
+		vim.cmd("0r /var/db/repos/gentoo/header.txt | $d | 0")
 	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
 	pattern = "metadata.xml",
-	command = "0r ~/Templates/metadata.xml",
+	command = "r ~/Templates/metadata.xml | 0d",
 })
 
 -- bash shebang
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
 	pattern = "*.sh",
-	command = "0r ~/Templates/skeleton.sh",
+	command = "r ~/Templates/skeleton.sh | 0d",
 })
